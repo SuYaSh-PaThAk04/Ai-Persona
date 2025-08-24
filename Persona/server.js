ef7396b841e13712e507af72b0ab0c6f0ab7c6a2
Suyash Pathak <suyashp271@gmail.com>
2025-08-24 19:50:09 +0530

    Update server.js

--- Persona/server.js
+++ Persona/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://ai-persona-two.vercel.app/'
+    'https://ai-persona-two.vercel.app'
   ],
   credentials: true
 }));
